d452de8d0ff858a0a2d30f4984092d4c3c10888d
mynameisgithub@gmail.com <onlyforgithub1@gmail.com>
2024-12-22 20:23:11 +0900

    change base
    change the base project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
-import Weather from './Weather';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to WeatherSpot!</h1>
-      <p>Start building your app here.</p>
-      <Weather/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/Weather.js
+++
@@ -1,7 +0,0 @@
-function Weather() {
-    return <h2>Weather DashBoard</h2>;
-}
-
-export default Weather;
-
-
