ef16c0a0fbefc8af2cb51a8bd34da6f1564c6e8f
mynameisgithub@gmail.com <onlyforgithub1@gmail.com>
2024-12-22 21:32:18 +0900

    connect firebase
    connect firebase successfuly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import MapComponent from './MapComponent';
+import { firestore } from "./firebase";
+import { useEffect } from 'react';
 
 function App() {
+  // Firestore와 관련된 동작을 useEffect에서 실행
+  useEffect(() => {
+    console.log("Firestore 인스턴스:", firestore);
+
+    // 예를 들어, Firestore 컬렉션 데이터 가져오기
+    // 아래는 Firestore에서 데이터를 읽는 예제입니다.
+    // import { collection, getDocs } from "firebase/firestore";
+    // const fetchData = async () => {
+    //   const querySnapshot = await getDocs(collection(firestore, "your-collection-name"));
+    //   querySnapshot.forEach((doc) => {
+    //     console.log(`${doc.id} =>`, doc.data());
+    //   });
+    // };
+    // fetchData();
+
+  }, []); // 빈 배열을 의존성으로 추가하여 useEffect가 한 번만 실행되도록 설정
+
   return (
-    <div>
+    <div className="App">
       <h1>에어스팟</h1>
       <MapComponent />
     </div>
@@ -11,4 +30,3 @@ function App() {
 }
 
 export default App;
-
