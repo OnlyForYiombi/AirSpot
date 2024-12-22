af2e391f78dce59208ac6da3b7a1ff36e550729f
mynameisgithub@gmail.com <onlyforgithub1@gmail.com>
2024-12-22 21:11:42 +0900

    add firebase
    add and connect firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAHtNO9Bh72Sc03tcvt9qSxJgWpdV5D5A",
+  authDomain: "airspot-80dec.firebaseapp.com",
+  projectId: "airspot-80dec",
+  storageBucket: "airspot-80dec.firebasestorage.app",
+  messagingSenderId: "855533275611",
+  appId: "1:855533275611:web:41a6013a44e1a0a0f57ffe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
